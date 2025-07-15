d344334f5ddbee79219c8dee7e15eab097880047
Awais Tabassum <awaistabassum25@gmail.com>
2025-07-15 06:08:38 +0000

    dataTypes

---
+++ 01_basics/vaiables.js
@@ -0,0 +1,11 @@
+
+
+const Awais=12345   //  value change nhi ho sakty
+let ali = "ali" 
+let ateeq = "anees"   // value change ho jati ha
+
+// Awais = 6789
+ali = "rehman"
+ateeq = "adress"
+// console.log(Awais)
+console.table([ali,ateeq])
---
+++ 02_dataTypes/dataTypes.js
@@ -0,0 +1,18 @@
+//"use strict";  for use newely java script
+
+//alert(3+4)  not work in node js only work on brower side
+// 
+//ECMS Script...(is a organization that define standards for js)
+
+
+//Number
+//bigint
+//String = ""
+//boolean = true/false
+//null = standalone value
+//undefine
+//symbol => unique
+//object
+
+//console.log(typeof undefined) => undefined
+//console.log(typeof null) => object
