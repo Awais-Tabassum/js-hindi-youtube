24ccce71b06ee44a2191316163e095fab74d57d5
Awais Tabassum <awaistabassum25@gmail.com>
2025-07-21 05:58:17 +0000

    VIDEO 8

---
+++ 03_dataTypeConversionOperations/test3.js
@@ -0,0 +1,32 @@
+let score = "Awais"
+let valueInNumbers = Number(score)
+//console.log(valueInNumbers); // => NaN
+//console.log(typeof(valueInNumbers)); // => number
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1, false => 0
+
+
+//fOR BOOLEAN DATATYPES:
+
+// 1 =>true, 0 =>false
+// "" =>false"
+//"Awais" => true
+
+
+//let a,b = 6;
+//b= ++a;
+//console.log(a);
+//console.log(b);
+
+
+//console.log(null > 0)  //false
+//console.log(null == 0)  //false
+//console.log(null >= 0)   //true
+
+
+//console.log(undefined > 0)    false
+//console.log(undefined >= 0)    false
+//console.log(undefined = 0)   false    AVOID THESE TYPE OF COMPARISIONS
